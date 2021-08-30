aa3a402a7685a89a4c3e6850bb3921c3f24b303e
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-08-30 17:12:01 +0200

    commit

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+
+1- dai un nome al panino
+
+2- fissa un prezzo base
+
+3- dai un valore uguale a tutti gli ingedienti
+
+4- calcola una somma per tutte le volte che si fa check su gli ingredienti
+
+5- chiedi un codice sconto
+
+6- verifica se è presente nell'array dei codici sonto
+
+7- somma tutto e stampa il prezzo finale
+
+*/
+
+var prezzoBase = 10;
+
+let nomeBurger = document.getElementById('name-burger').innerHTML=(prompt('dai un nome al panino'));
+
+for ()
+
+
