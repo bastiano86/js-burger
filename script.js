35a7fcd1e7cad582dd9cf3c743cf56ae0f2ce4f4
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-08-31 09:29:47 +0200

    Esercizio completato

--- script.js
+++ script.js
@@ -16,10 +16,34 @@
 
 */
 
-var prezzoBase = 10;
 
-let nomeBurger = document.getElementById('name-burger').innerHTML=(prompt('dai un nome al panino'));
 
-for ()
+let nomeBurger = document.getElementById('nome-panino');
+console.log(nomeBurger);
+
+let bottone = document.getElementById('bottone');
+console.log(bottone);
+
+bottone.addEventListener('click', function() {
+    if (nomeBurger.value.length === 0){
+        alert('scrivi un nome per il panino');
+    }   else { 
+        var prezzoBase = 10;
+        
+        let ingredientiAgg = document.getElementsByClassName('ing-check');
+        console.log(ingredientiAgg);
+
+        for (let i = 0; i < ingredientiAgg.length; i++) {
+            let ingredienteAtt = ingredientiAgg[i];
+            console.log(ingredienteAtt);
+
+            if (ingredienteAtt.checked) {
+                prezzoBase += 7.5;
+            }
+        }
+        console.log(prezzoBase);
+        document.getElementById("final-price").innerHTML = (prezzoBase);
+    } 
+});
 
 
